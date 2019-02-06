e3ef538a8905f3180d47c92d7a50050e8925fda2
Brian Speight <bdomercial@me.com>
2019-02-05 21:00:02 -0800

    Displaying the conversaiont prop

--- components/VNextElements/Conversation.js
+++ components/VNextElements/Conversation.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import ConvoContainer from '../utilities/globalStyles/ConversationStyles';
+import React, { Component } from "react";
+import ConvoContainer from "../utilities/globalStyles/ConversationStyles";
 
 export default class Conversation extends Component {
   render() {
     return (
       <ConvoContainer>
-        <p>hello there</p>
+        <p>{this.props.convers}</p>
       </ConvoContainer>
     );
   }
