544022494f0ab741ad1abc5ec2a42a32a9fc88df
Brian Speight <brspeight@expedia.com>
2019-02-15 14:34:17 -0800

    Moving ConversationPane styles to their own file and importing into bot and chat files.

--- components/VNextElements/BotConversation.js
+++ components/VNextElements/BotConversation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import ConversationPane from './ConversationPane';
 
 let chatCounter = 0;
 let chatTimer = null;
@@ -37,77 +37,3 @@ export default class BotConversation extends Component {
     );
   }
 }
-
-const ConversationPane = styled.div`
-  padding: 16px;
-  flex-grow: 2;
-  overflow-y: auto;
-  .message {
-    position: relative;
-    list-style: none;
-    padding: 0;
-    margin: 0 0 8px 0;
-    .thought {
-      display: flex;
-    }
-    .thought span {
-      border-radius: 0;
-      margin-bottom: 2px;
-      padding: 12px 16px;
-      max-width: 75%;
-    }
-  }
-  .incoming:before {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    content: '';
-    background-image: url('../../static/images/icn-chatBot.svg');
-    height: 24px;
-    width: 24px;
-  }
-  .message.incoming {
-    padding-left: 32px;
-    .thought span {
-      background-color: #f0f0f2;
-    }
-    .thought:only-child span {
-      border-radius: 8px 8px 8px 0;
-    }
-    .thought:first-child span {
-      border-radius: 8px 8px 0 0;
-    }
-    .thought:last-child span {
-      border-radius: 0 0 8px 0;
-    }
-  }
-  .message.outgoing:after {
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    content: '';
-    background-image: url('../../static/images/icn-chatBot.svg');
-    height: 24px;
-    width: 24px;
-  }
-  .message.outgoing {
-    padding-right: 32px;
-    .thought {
-      flex-direction: row-reverse;
-    }
-    .thought span {
-      /* background-color: #dbdbf1; */
-      background-color: #f0f0f2;
-      display: inline-block;
-    }
-    .thought:only-child span {
-      border-radius: 8px 8px 0 8px;
-    }
-    .thought:first-child span {
-      border-radius: 8px 8px 0 0;
-    }
-    .thought:last-child span {
-      border-radius: 0 0 0 8px;
-    }
-  }
-`;
---
+++ components/VNextElements/ConversationPane.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+const ConversationPane = styled.div`
+  padding: 16px;
+  flex-grow: 2;
+  overflow-y: auto;
+  .message {
+    position: relative;
+    list-style: none;
+    padding: 0;
+    margin: 0 0 8px 0;
+    .thought {
+      display: flex;
+    }
+    .thought span {
+      border-radius: 0;
+      margin-bottom: 2px;
+      padding: 12px 16px;
+      max-width: 75%;
+    }
+  }
+  .incoming {
+    padding-left: 32px;
+  }
+  .incoming:before {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    content: '';
+    background-image: url('../../static/images/icn-chatBot.svg');
+    height: 24px;
+    width: 24px;
+  }
+  .outgoing {
+    padding-right: 32px;
+  }
+  .outgoing:after {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    content: '';
+    background-image: url('../../static/images/icn-chatBot.svg');
+    height: 24px;
+    width: 24px;
+  }
+  .outgoing .thought {
+    flex-direction: row-reverse;
+  }
+  .message {
+    .thought span {
+      background-color: #ffffff;
+    }
+    .thought:first-child span {
+      border-radius: 8px 8px 0 0;
+    }
+    .thought:last-child span {
+      border-radius: 0 0 8px 0;
+    }
+    .thought:only-child span {
+      border-radius: 8px 8px 8px 0;
+    }
+  }
+`;
+
+export default ConversationPane;
